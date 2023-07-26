a9cdcf9dc03c4fc0fac5c3248787b92061f6acae
nbogie <neillbogie@googlemail.com>
2023-07-26 13:14:01 +0100

    initial exercise

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+    return (
+        <main className="App">
+            Think of an animal from this list and I will try to guess it!
+            <div>TODO: your stuff goes here</div>
+            <button>Restart</button>
+        </main>
+    );
+}
