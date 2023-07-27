8f1907ed38f925d7ce43e65b66853b790f631ae8
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-27 10:36:10 +0100

    Added button and state for traversing quiz tree

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,37 @@
 import "./App.css";
+import { useState } from "react";
+import createTree from "../exampleTrees/animalsQuizTree";
+
+const questionTree = createTree();
 
 export default function App() {
+  const [currentNode, setCurrentNode] = useState(questionTree);
+
+  const handleClick = (value: "yes" | "no") => {
+    setCurrentNode((n) => {
+      if (n.kind === "question") {
+        return value === "yes" ? n.yesSubtree : n.noSubtree;
+      }
+
+      return n;
+    });
+  };
+
   return (
     <main className="App">
       Think of an animal from this list and I will try to guess it!
-      <div>TODO: your stuff goes here</div>
-      <button>Restart</button>
+      <div>
+        {currentNode.kind === "question"
+          ? currentNode.question
+          : currentNode.result}
+      </div>
+      {currentNode.kind === "question" && (
+        <div>
+          <button onClick={() => handleClick("yes")}>Yes</button>
+          <button onClick={() => handleClick("no")}>No</button>
+        </div>
+      )}
+      <button onClick={() => setCurrentNode(questionTree)}>Restart</button>
     </main>
   );
 }
